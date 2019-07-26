22f5b798e64183f8ad83d3d8b8bbf1b2c3120df5
ThT <tuan.tranho@kyanon.digital>
2019-07-26 11:50:39 +0700

    done

--- instagram-feed/App.js
+++ instagram-feed/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, Button } from 'react-native';
-import { Feather, MaterialIcons } from '@expo/vector-icons';
+import { StyleSheet, Text, View, Image, ScrollView, Button, TouchableOpacity } from 'react-native';
+import { Feather, MaterialIcons, Entypo } from '@expo/vector-icons';
 
 export default function App() {
   return (
@@ -8,7 +8,10 @@ export default function App() {
       <View style={styles.dropLayout}>
       </View>
       <View style={styles.navigation}>
-        <Feather name="arrow-left" size={40} color="grey" />
+        <Feather name="arrow-left" size={30} color="grey" />
+        <Image
+          source={require('./assets/icon_1.png')}
+        />
       </View>
       <View style={styles.header}>
         <View style={styles.headerLogo}>
@@ -40,13 +43,15 @@ export default function App() {
               />
             </View>
             <View style={styles.buttonSend}>
-              <MaterialIcons
-                name="send"
-                size={22}
-                color="white"
-                style={styles.shadowColor}
-                onPress={() => { alertMessage('send') }}
-              />
+              <TouchableOpacity style={styles.FacebookStyle} activeOpacity={0.5}>
+                <MaterialIcons
+                  name="send"
+                  size={22}
+                  color="white"
+                  style={styles.shadowColor}
+                  onPress={() => { alertMessage('send') }}
+                />
+              </TouchableOpacity>
             </View>
           </View>
         </View>
@@ -54,7 +59,7 @@ export default function App() {
       <View style={styles.totalArea}>
         <View style={styles.photosTotal}>
           <View>
-            <Text style={styles.totalNumber}> 210 </Text>
+            <Text style={styles.totalNumber}> 9 </Text>
           </View>
           <View>
             <Text style={styles.totalText}>Photos</Text>
@@ -62,7 +67,7 @@ export default function App() {
         </View>
         <View style={styles.followersTotal}>
           <View>
-            <Text style={styles.totalNumber}>15k</Text>
+            <Text style={styles.totalNumber}> 21 </Text>
           </View>
           <View>
             <Text style={styles.totalText}>Followers</Text>
@@ -70,7 +75,7 @@ export default function App() {
         </View>
         <View style={styles.followingTotal}>
           <View>
-            <Text style={styles.totalNumber}>605</Text>
+            <Text style={styles.totalNumber}> 1997 </Text>
           </View>
           <View>
             <Text style={styles.totalText}>Following</Text>
@@ -97,8 +102,16 @@ export default function App() {
           </ScrollView>
         </View>
       </View >
+      <View style={styles.dropLayoutFooter}>
+      </View>
       <View style={styles.footer}>
-
+        <Image
+          source={require('./assets/icon_2.png')}
+        />
+        <Image
+          source={require('./assets/icon_3.png')}
+        />
+        <Feather name="user" size={30} color="grey"></Feather>
       </View>
     </View >
   );
@@ -143,6 +156,9 @@ const imgData = [
   { id: 4, imgSource: require('./assets/4.jpeg') },
   { id: 5, imgSource: require('./assets/5.jpeg') },
   { id: 6, imgSource: require('./assets/6.jpeg') },
+  { id: 7, imgSource: require('./assets/b.jpg') },
+  { id: 8, imgSource: require('./assets/c.jpg') },
+  { id: 9, imgSource: require('./assets/4.jpeg') },
 ];
 
 const styles = StyleSheet.create({
@@ -154,7 +170,11 @@ const styles = StyleSheet.create({
     flex: 0.030,
   },
   navigation: {
+    marginHorizontal: 12,
+    marginVertical: 10,
     flex: 0.070,
+    justifyContent: 'space-between',
+    flexDirection: 'row',
   },
   header: {
     flex: 0.2,
@@ -172,8 +192,15 @@ const styles = StyleSheet.create({
     marginHorizontal: 12,
     flexDirection: 'row',
   },
+  dropLayoutFooter: {
+    flex: 0.02,
+  },
   footer: {
-    flex: 0.075,
+    flex: 0.055,
+    justifyContent: 'space-around',
+    marginHorizontal: 12,
+    marginVertical: 10,
+    flexDirection: 'row',
   },
   //header
   headerLogo: {
@@ -204,6 +231,7 @@ const styles = StyleSheet.create({
   textName: {
     fontSize: 35,
     fontWeight: 'bold',
+    color: 'rgb(51,60,87)',
   },
   textTitle: {
     color: 'grey',
@@ -250,6 +278,7 @@ const styles = StyleSheet.create({
   totalNumber: {
     fontSize: 25,
     fontWeight: 'bold',
+    color: 'rgb(51,60,87)',
   },
   totalText: {
     fontSize: 15,
